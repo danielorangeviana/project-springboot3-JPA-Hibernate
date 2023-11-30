40fb57dced5a75b56777c18d962b23ef26913a6f
danielorangeviana <danieloviana@gmail.com>
2023-11-30 20:01:12 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/programmerorange/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.programmerorange.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.programmerorange.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
