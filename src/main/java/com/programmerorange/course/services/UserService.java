5238c3582bbe025998a8f36bde7f0bd107902fea
danielorangeviana <danieloviana@gmail.com>
2023-12-04 19:06:26 -0300

    Service layer, component registration

---
+++ src/main/java/com/programmerorange/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.programmerorange.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.programmerorange.course.entities.User;
+import com.programmerorange.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
