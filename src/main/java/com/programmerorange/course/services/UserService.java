ac400a44a6a5db4e39a46f54a893726ebda33762
danielorangeviana <danieloviana@gmail.com>
2024-01-11 15:38:58 -0300

    Exception handling - update

--- src/main/java/com/programmerorange/course/services/UserService.java
+++ src/main/java/com/programmerorange/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.programmerorange.course.repositories.UserRepository;
 import com.programmerorange.course.services.exceptions.DatabaseException;
 import com.programmerorange.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -40,9 +42,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
